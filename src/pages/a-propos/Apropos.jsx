05baea699ac73e95153cd2a6ad86f50912cd7e72
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-11-08 12:16:04 +0100

    ajout text apropos

--- src/pages/a-propos/Apropos.jsx
+++ src/pages/a-propos/Apropos.jsx
@@ -26,6 +26,30 @@ const Apropos = (props) => {
             </div>
           </section>
 
+          <section className={`apropos-button`}>
+            <div className=''>
+              <p className='apropos-writing'>Respect<i class={`fa-solid fa-chevron-down hidden ${isRotated ? 'rotated' : ''}`} onClick={showContent}></i></p>
+              {aproposContent &&
+              <p className='apropos-button-content'>La bienveillance fait partie des valeurs fondatrices de Kasa.Tout comportement discriminatoire ou de perturbation de voisinage entrainera une exclusion de notre plateforme.</p>}
+            </div>
+          </section>
+
+          <section className={`apropos-button`}>
+            <div className=''>
+              <p className='apropos-writing'>Service<i class={`fa-solid fa-chevron-down hidden ${isRotated ? 'rotated' : ''}`} onClick={showContent}></i></p>
+              {aproposContent &&
+              <p className='apropos-button-content'>La bienveillance fait partie des valeurs fondatrices de Kasa.Tout comportement discriminatoire ou de perturbation de voisinage entrainera une exclusion de notre plateforme.</p>}
+            </div>
+          </section>
+
+          <section className={`apropos-button`}>
+            <div className=''>
+              <p className='apropos-writing'>Sécurité<i class={`fa-solid fa-chevron-down hidden ${isRotated ? 'rotated' : ''}`} onClick={showContent}></i></p>
+              {aproposContent &&
+              <p className='apropos-button-content'>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que vous les voyageurs, chaque logements correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>}
+            </div>
+          </section>
+
        {/*<div className='apropos-container'>
           <p className='apropos-writing'>Respect</p>
           <button className='apropos-button'>
