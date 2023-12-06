241eb5e1b3f3ad741222fa78d648a84f3f86239a
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-12-06 10:27:29 +0100

    p8 final

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <img src='assets/Logo-footer.png' alt='Logo' />
+        <p className='copyright'><i class="fa-regular fa-copyright"></i>2020 Kasa. All right reserved</p>
     </div>
   )
 }
